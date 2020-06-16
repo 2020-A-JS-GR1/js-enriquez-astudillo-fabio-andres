69ec8a002f34894661304c08f16ab3d6a3789daf
Andrés Enríquez <fabio.and1514@gmail.com>
2020-06-16 17:59:39 -0500

    objetos

--- 01-javascript/01-variables.js
+++ 01-javascript/01-variables.js
@@ -61,10 +61,93 @@ const objetojs = {
     mascotas: ['Rex', 'Max', 'David']
 }; //object
 
+
+
 const arreglo = []; //object
 
 console.log(objetojs);
 console.log(objetojs.mascotas);
+console.log(objetojs["nombre"]);
 console.log(arreglo);
 
+objetojs.apellido = "astudillo";
+console.log(objetojs.apellido);
 
+objetojs["apellido"] = "enriquez";
+console.log(objetojs.apellido);
+
+objetojs.sueldo;
+console.log(objetojs.sueldo);
+
+objetojs.sueldo = 1.2;
+console.log(objetojs.sueldo);
+
+objetojs["gastos"];
+console.log(objetojs["gastos"]);
+
+objetojs["gastos"] = 8;
+console.log(objetojs["gastos"]);
+
+objetojs.sueldo = undefined;
+console.log(objetojs);
+console.log(Object.keys(objetojs));
+console.log(Object.values(objetojs));
+
+//eliminar completamente una llave dentro de un objeto
+delete objetojs.sueldo;
+console.log(objetojs);
+
+console.log(Object.keys(objetojs));
+console.log(Object.values(objetojs));
+
+const arrkeys = Object.keys(objetojs);
+console.log(arrkeys[2]);
+const arrvalues = Object.values(objetojs);
+console.log(arrvalues[2]);
+
+objetoVacio = {}
+console.log(Object.values(objetoVacio));
+
+console.log('\n\n\n\n\n\n\n\n');
+
+//Lista de variables por valor
+//number
+//string
+//boolean
+//undefined
+
+let edadFabio = 23;
+let edadAndres = edadFabio;
+edadFabio += 1;
+console.log(edadFabio);
+console.log(edadAndres);
+
+//Lista de variables por referencia
+//objects
+
+let fabio = {
+    nombre: "Fabio"
+}
+let andres = fabio;
+fabio.nombre = "Andres"
+console.log(fabio);
+console.log(andres);
+delete fabio.nombre;
+console.log(fabio);
+console.log(andres);
+
+let fabioarray = ["fabio", "andres"]
+let andresarray = fabioarray;
+fabioarray[1] = "Enriquez"
+console.log(fabioarray);
+console.log(andresarray);
+
+//forma de copiar un objeto sin apuntar al mismo espacio de memoria
+
+let rafael = {
+    nombre: "Rafael"
+};
+let lenin = Object.assign({},rafael);
+delete rafael.nombre;
+console.log(rafael);
+console.log(lenin);
