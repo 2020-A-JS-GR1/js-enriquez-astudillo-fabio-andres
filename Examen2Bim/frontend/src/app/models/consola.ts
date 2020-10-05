690f52c6677f32b6cec70be30735eec7d63e13b3
Andrés Enríquez <fabio.and1514@gmail.com>
2020-10-05 00:22:08 -0500

    fin

--- Examen2Bim/frontend/src/app/models/consola.ts
+++ Examen2Bim/frontend/src/app/models/consola.ts
@@ -1,7 +1,10 @@
+import { Videojuego } from './videojuego';
+
 export interface Consola {
     id?: number;
     Nombre: string;
     Fabricante: string;
     Generacion: number;
     tieneSoporte: boolean;
+    videojuegos?: number[];
 }
--- Examen2Bim/frontend/src/app/routes/editar-consola/editar-consola.component.ts
+++ Examen2Bim/frontend/src/app/routes/editar-consola/editar-consola.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Consola } from 'src/app/models/consola';
 import { ConsolaService } from 'src/app/services/consola.service';
+import { VideojuegoService } from 'src/app/services/videojuego.service';
 
 @Component({
   selector: 'app-editar-consola',
@@ -17,7 +18,8 @@ export class EditarConsolaComponent implements OnInit {
   constructor(
     private readonly router: Router,
     private readonly route: ActivatedRoute,
-    private readonly consolaService: ConsolaService
+    private readonly consolaService: ConsolaService,
+    private readonly videojuegoService: VideojuegoService
   ) { }
 
   ngOnInit(): void {
@@ -38,15 +40,23 @@ export class EditarConsolaComponent implements OnInit {
   }
 
   editar(consola: Consola) {
-    this.consolaService.editConsola(this.consolaID, consola).subscribe(
-      consola => {
-        alert('Consola editada con éxito');
-        this.router.navigate(['/consola']);
-      },
+    this.videojuegoService.getVideojuegos(this.consolaID).subscribe(
+      videojuegos => {
+        consola.videojuegos = videojuegos.map(n=>n.id);
+        this.consolaService.editConsola(this.consolaID, consola).subscribe(
+          consola => {
+            alert('Consola editada con éxito');
+            this.router.navigate(['/consola']);
+          },
+          error => {
+            console.error(error);
+          }
+        )
+      }, 
       error => {
         console.error(error);
       }
-    )
+    );
   }
 
   regresar() {
